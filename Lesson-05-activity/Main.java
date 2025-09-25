718598e7d745b62594e44c5b907a3813fbec5953
Nicolea01 <nicolea261@nycstudents.net>
2025-09-25 13:26:42 +0000

    update

---
+++ Lesson-05-activity/Main.java
@@ -0,0 +1,105 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+    
+
+/*  
+    Challenge 1:
+    Create two integer variables and Assign values to them. 
+    Calculate the sum of the two numbers and store the 
+    calculated sum and then display it.
+    
+*/
+   int x = 1, y = 2;
+   int sum = x + y;
+   System.out.println(sum);
+
+/*  
+    Challenge 2:
+    Create three variables to assign three grades and Assign values to each grade. 
+    Calculate the sum of the three grades and store the 
+    calculated sum and then display it.
+    
+*/
+    int g1 = 85, g2 = 90, g3 = 76;
+    int sum1 = g1 + g2 + g3;
+    System.out.print(sum1);
+
+
+/*  
+    Challenge 3:
+    Calculate the average from the three grades from challenge 2,
+    store the value and then display it.
+    Declare and assign values to any new variables
+    NOTE: Does it look correct, check with a calculator?
+*/
+    int avg = (g1 + g2 + g3) / 3;
+    System.out.println(avg);
+
+
+/*  
+    Challenge 4:
+    Write the following equation in EQ1.PNG file in Java; store the result and the display it:
+    Declare and assign values to any new variables
+
+*/
+    int A = 5, x = 3;
+    double y = (A)/(x+1);
+    System.out.println(y1);
+
+/*  
+    Challenge 5:
+    Using the variables same variables from challenge4 above, write the following equation in EQ2.PNG file in Java, store the result and the display it:
+
+    Declare and assign values to any new variables
+
+*/
+ 
+    double y2 = (2*x1(x+1))(-x1/2);/A;
+    System.out.println(y2);
+
+
+
+/*  
+    Challenge 6:
+    Create the variables and write the equation in
+    file  EQ3.PNG
+
+    Declare and assign values to any new variables
+*/
+    int b = 10, h = 20;
+    double area = (1/2)*b*h;
+    System.out.println(area);
+
+
+
+
+
+/*  
+    **** Bonus Challenge ****:
+    Create a variable that stores the total number of eggs 
+    and assign it 100. We want to fill as many baskets with 
+    eggs as we can. Each basket can hold only 12 eggs.
+
+    1) Write the java code that will calcute how many baskets
+    of 12 eggs can we fill fully.
+
+    HINT: What do we get when we divide an integer by 
+    an integer in Java
+
+    2) Write the java code that will calculate how many eggs
+    are left over after we filled as many baskets of 12 eggs.
+*/
+
+
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
---
+++ Lesson-07-activity/Main.java
@@ -0,0 +1,115 @@
+
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+/*  
+    Challenge 1:
+    1) Create the variables, ask the user for the variable values, write the equation in file EQ1-act6 and display the equation value.
+*/
+  System.out.println("Enter x");
+  double x = Input.readDouble();
+  double y= o;
+  y = Math.pow.(x,7);
+  System.out.println(y);
+
+/*  
+    Challenge 2:
+    1) Create the variables, ask the user for the variable values, write the equation in fileEQ1.1-act6 and display the equation value.
+*/
+  System.out.println("Enter z");
+  double x = Input.readDouble();
+  double y = Math.pow.(z, 3)+5;
+  System.out.println("#2 is" + q);
+
+/*  
+    Challenge 3:
+    Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
+    
+*/
+  System.out.println("Enter r");
+  double r = Input.readDouble();
+  System.out.println("Enter t");
+  double t = Input.readDouble();
+  double s = Math.pow.(x, 5);
+  System.out.println("#2 is" + 5);
+ 
+
+/*  
+    Challenge 4:
+    Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
+    
+*/
+  System.out.println("Enter A");
+  double r = Input.readDouble();
+  System.out.println("Enter B");
+  double t = Input.readDouble();
+  double s = Math.pow.(A + B);
+  System.out.println("#4 is" + c);
+
+
+/*  
+    Challenge 5:
+    Create the variables, ask the user for the variable values, write the equation in file EQ4-act6 and display the equation value..
+    
+*/
+   System.out.println("Enter x1");
+  double x1 = Input.readDouble();
+  System.out.println("Enter x2");
+  double x2 = Input.readDouble();
+  System.out.println("Enter y1");
+  double y1 = Input.readDouble();
+  System.out.println("Enter y2");
+  double x1 = Input.readDouble();
+  double d = (Math.sqrt.(x2-x1, 2)+Math.pow(y2-y1, 2));
+  System.out.println("#4 is" + y);
+
+
+
+/*  
+    Challenge 6:
+    Create the variables, ask the user for the variable values, write the equation g=sin(deg) and display the equation value..
+    
+*/
+  System.out.println("Enter Degree");
+  double degree = Input.readDouble();
+  double g = Math.sin(degre);
+  System.out.println("#6 is" +g);
+
+
+
+
+/*  
+    Challenge 7:
+    Create the variables, ask the user for the variable values, write the equation in file EQ5-act6 and display the equation value.
+    
+*/
+  System.out.println("Enter m");
+  double m = Input.readDouble();
+  System.out.println("Enter n");
+  double n = Input.readDouble();
+  System.out.println("Enter n");
+  double k = Math.pow(m, 6) / Math.sqrt(n+1);
+  System.out.println("#7 is" +k);
+  
+
+
+
+/*  
+    *** Bonus Challenge ***:
+    Create the variables, ask the user for the variable values, write the equation in file Ch-act6 and display the equation value.
+
+    HINT: What does the "plus minus: after "-b" mean.
+*/
+
+
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
