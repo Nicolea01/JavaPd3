83a47281f8d26deffe98dcd3ebed3d734c5b8ddf
Nicolea01 <nicolea261@nycstudents.net>
2025-09-30 23:01:22 +0000

    data

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,42 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void print(String word){
+	System.out.println(word);
+
+  }
+
+  double FtoC(double f){
+	double c = (F-32)*9.0/5;
+	return c;
+  }
+   void init(){
+	print("Hello");
+	FtoC(32);
+}
+
+void print(string msg){
+    system.out.println(msg);
+}
+
+double f to c (double f){
+    double c =(f-32)9/5.0;
+    return c;
+}
+
+double sphereVolume (double volume){
+    double volume = (4/3.0) * Math.PI* r*r*r;
+    return v; 
+}
+
+double coneVolume (double volume){
+volume = (1/3) * math.pi * (radius**2) * height;
+  return volume;
+}
+
+double dist = distance(point1X, point1Y, point2X, point2Y);
+        System.out.println("The distance between (" + point1X + "," + point1Y + ") and (" + point2X + "," + point2Y + ") is: " + dist);
+    }
