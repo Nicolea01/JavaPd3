d0ba5ae6ea7f99c93c280cdd8fc2a83c9172c383
Nicolea01 <nicolea261@nycstudents.net>
2025-11-07 14:23:10 +0000

    update

--- Lesson-19-Classwork/Main.java
+++ Lesson-19-Classwork/Main.java
@@ -5,9 +5,44 @@ class Main {
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
 
-  void init(){
+  String rollDice(int d1, int d2){
+    String roll = "";
+    d1 = (int)(Math.random()*(6)+1);
+    d2 = (int)(Math.random()*(6)+1);
+    return roll = d1 + "" + d2;
+  }
 
-    System.out.println();
+  String lotto(){
+    String lot = "";  
+    for(int x = 0; x < 5; x++){
+      int num = (int)(Math.random()*(48)+1);
+      lot += num + " ";
+    }
+   return lot;
+  }
+
+  String additionTutor(){
+    String equation = " "; 
+    int num1 = (int)(Math.random()*(100)+1);
+    int num2 = (int)(Math.random()*(100)+1);
+    equation = num1 + " " + "+" + " " + num 2;
+    System.out.println("What is the sum?");
+    int user = Input.readInt(); 
+    String ans = num1 + num2; 
+    if (user.equals(ans)){
+      return "Correct"; 
+    }else{
+      return "Incorrect";
+     }
+    }
+    
+
+  }
+
+  void init(){
+    System.out.println(rollDice());
+    System.out.println(lotto());
+    System.out.println(additionTutor());
 
    
   }
@@ -18,4 +53,3 @@ class Main {
   }
 
   
-}
