b0e36bb6e7c6d21293a0d1cbf0854b521ed2031f
Nicolea01 <nicolea261@nycstudents.net>
2025-11-22 00:01:18 +0000

    q21

--- Lesson-21-Activity/Main.java
+++ Lesson-21-Activity/Main.java
@@ -7,34 +7,65 @@ class Main {
   void print(Object o){System.out.println(o);}
   void printt(Object o){System.out.print(o);}
 
-   double allowance(){
-  String days = "";
-  String allowance(){
-    int days = 0;
-    double penny = 0.01;
-    double amount = penny*2;
-    while(!(amount==5)){
-      days++;
+    double total(double[] prices){
+    double sum = 0;
+    for(int i = 0; i < prices.length; i++){
+      sum += prices[i];
     }
-    return days;
-    return "It would take " + days + " to reach $5.";
+    return sum*1.08525;
   }
 
-  int addTwoNumbers(){
-    int num1 = randInt(1,100);
-    int num2 = randInt(1,100);
-    int equ = num1 + num2;
-    System.out.println(equ + "=?");
-    int guess = Input.readInt();
-    while()
+  boolean isin(int[]array, int num){
+    for(int i = 0; i < array.length; i++){
+      if(array[i] == num){
+        return true;
+      }
+      return false;
+  }
 
+  int checkLotto(int[]picks, int[]drawn){
+    int check = 0;
+    for(int i = 0; i < picks.length; i++){
+      if(isin(drawn, picks[i])){
+        check++;
+      }
+    }
+    return check;
   }
 
   void init(){
-    allowance();
-    
+    double[] price = {2.50, 8.99, 3.99, 10.25};
+    print(total(price)); //$27.92
+
+    int[]test={1,2,3,4,5,6,7,8};
+    print(isin(test, 1)); //true
+
   }
 
+  // Use these functions below for the activity
+
+  String convertToEmoji(String code){
+    return String.valueOf(Character.toChars(
+                               Integer.parseInt(code, 16)));
+  }
+
+  
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+  boolean isPrime(int n){
+    int divisor = 2;
+    while(n%divisor !=0){
+      divisor+=1;
+    }
+    if(divisor==n)
+      return true;
+    else
+      return false;
+  }
+}
 
 
   
---
+++ Lesson-23-Activity/Main.java
@@ -0,0 +1,68 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){System.out.println(o);}
+  void printt(Object o){System.out.print(o);}
+
+  double total(double[] prices){
+    double sum = 0;
+    for(int i=0; i < prices.length; i++){
+      sum += prices[i];
+    }
+    return sum*1.08525;
+  }
+
+  String isin(int[]array, int num){
+    for(int i=0; i < prices.length; i++){
+      if(array[i] == num){
+        return "true";
+      }else{
+        result = "false";
+      }
+    }
+    result result;
+  }
+
+  }
+
+  void init(){
+    double[] price = {2, 8, 3.99, 10};
+    print(total(price));//$27.92
+
+    int[] test = {1, 2, 3, 4, 5,6,7,8};
+    print(isin(test, 1));//true
+  }
+
+  int checkLotto(int[]picks, int[]drawn){
+    int check = 0;
+    for(int i = 0; i < picks.length; i++){
+  
+    }
+  }
+
+
+
+  String convertToEmoji(String code){
+    return String.valueOf(Character.toChars(
+       Integer.parseInt(code, 16)));
+  }
+
+  
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+  boolean isPrime(int n){
+    int divisor = 2;
+    while(n%divisor !=0){
+      divisor+=1;
+    }
+    if(divisor==n)
+      return true;
+    else
+      return false;
+  }
