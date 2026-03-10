8b1a2bc239e98ff3b5b54d98d386124e3a8c04a5
Nicolea01 <nicolea261@nycstudents.net>
2026-03-10 13:31:23 +0000

    a40

--- Lesson-40-Activity/Main.java
+++ Lesson-40-Activity/Main.java
@@ -23,10 +23,20 @@ class Main {
 
     String sql0 ="Select * From cr101 Where teacher1 = 'PORCHETTA' AND Period = '10';";
 
+    String sql2 = "Select studentId From cr101";
+
+    String sql3 ="Select studentId, course, period From cr101 Where room In('322', '105','106','323') AND Grade = '11';";
+
+     String sql4 ="Select * From cr101 Where course LIKE 'M%' AND course LIKE '%X';";
+
     Database db = new Database("jdbc:sqlite:students.db");
 
-    //queryResult = db.runSQL(sql, "table-auto");
+    queryResult = db.runSQL(sql, "table-auto");
     queryResult = db.runSQL(sql0, "table-auto");
+    queryResult = db.runSQL(sql2, "table-auto");
+    queryResult = db.runSQL(sql3, "table-auto");
+    queryResult = db.runSQL(sql4, "table-auto");
+
     print(queryResult);
 
    
